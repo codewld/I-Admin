f7bff06d1525e278e8769c4f1f181c3224adb912
codewld <wang_longda@outlook.com>
2022-08-07 16:55:16 +0800

    init[Server]

---
+++ I-Admin-Server/src/main/java/pers/codewld/iadmin/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package pers.codewld.iadmin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * Swagger配置类
+ */
+@Configuration
+@EnableOpenApi
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("pers.codewld.iadmin.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("I-Admin")
+                .description("I-Admin接口文档")
+                .build();
+    }
+
+}
---
+++ I-Admin-Server/src/test/java/pers/codewld/iadmin/IAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.codewld.iadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IAdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
