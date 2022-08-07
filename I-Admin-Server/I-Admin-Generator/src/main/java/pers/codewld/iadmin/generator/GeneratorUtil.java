0b7d7adfc62ced6cb7501134ee118ceb4f2c82b5
codewld <wang_longda@outlook.com>
2022-08-07 20:51:07 +0800

    feat[Server]: 调整MyBatis Plus Generator为手动输入配置

--- I-Admin-Server/I-Admin-Generator/src/main/java/pers/codewld/iadmin/generator/GeneratorUtil.java
+++ I-Admin-Server/I-Admin-Generator/src/main/java/pers/codewld/iadmin/generator/GeneratorUtil.java
@@ -6,83 +6,63 @@ import com.baomidou.mybatisplus.generator.config.OutputFile;
 import com.baomidou.mybatisplus.generator.config.po.LikeTable;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 import org.apache.ibatis.annotations.Mapper;
-import org.yaml.snakeyaml.Yaml;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Objects;
+import java.util.Scanner;
 
 /**
  * MyBatis Plus Generator工具类
  */
 public class GeneratorUtil {
 
-    private static HashMap<String, Object> configuration;
-
-    private static final String baseDir = System.getProperty("user.dir") + "/I-Admin-Generator";
-
-    static void loadConfigurations() throws IOException {
-        configuration = new Yaml().load(Files.newInputStream(Paths.get(baseDir + "/src/main/resources/config.yml")));
-    }
-
     public static void main(String[] args) {
-        try {
-            loadConfigurations();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入数据库连接URL： [jdbc:mysql://localhost:3306/i-admin]");
+        String url = scanner.nextLine();
+        System.out.println("请输入数据库用户名： [root]");
+        String username = scanner.nextLine();
+        System.out.println("请输入数据库密码： [root]");
+        String password = scanner.nextLine();
+        System.out.println("请输入父包名： [pers.codewld.iadmin.api]");
+        String parent = scanner.nextLine();
+        scanner.close();
 
-            String url = (String) configuration.get("url");
-            String username = (String) configuration.get("username");
-            String password = (String) configuration.get("password");
-            String parent = (String) configuration.get("parent");
-            Boolean isolated = (Boolean) configuration.get("isolated");
+        String baseOutputDir = System.getProperty("user.dir") + "/I-Admin-Generator/output";
+        String JavaOutputDir = baseOutputDir + "/java";
+        String XMLOutputDir = baseOutputDir + "/resources/mapper";
 
-            String baseOutputDir = baseDir;
-            if (Objects.equals(isolated, true)) {
-                baseOutputDir += "/output";
-            } else {
-                baseOutputDir += "/src/main";
-            }
-            String JavaOutputDir = baseOutputDir + "/java";
-            String XMLOutputDir = baseOutputDir + "/resources/mapper";
-
-            FastAutoGenerator.create(url, username, password)
-                    .globalConfig(builder ->
-                            builder
-                                    .disableOpenDir()
-                                    .enableSwagger()
-                                    .outputDir(JavaOutputDir))
-                    .packageConfig(builder ->
-                            builder
-                                    .parent(parent)
-                                    .entity("model.entity")
-                                    .pathInfo(Collections.singletonMap(OutputFile.xml, XMLOutputDir)))
-                    .strategyConfig(builder ->
-                            builder
-                                    .notLikeTable(new LikeTable("relation"))
-                                    .controllerBuilder()
-                                    .enableRestStyle()
-                                    .entityBuilder()
-                                    .enableLombok()
-                                    .enableRemoveIsPrefix()
-                                    .mapperBuilder()
-                                    .mapperAnnotation(Mapper.class)
-                                    .serviceBuilder()
-                                    .convertServiceFileName((entityName -> entityName + ConstVal.SERVICE)))
-                    .templateEngine(new FreemarkerTemplateEngine())
-                    .templateConfig(builder ->
-                            builder
-                                    .controller("/template/controller.java")
-                                    .entity("/template/entity.java")
-                                    .mapper("/template/mapper.java")
-                                    .xml("/template/mapper.xml")
-                                    .service("/template/service.java")
-                                    .serviceImpl("/template/serviceImpl.java"))
-                    .execute();
-        } catch (IOException e) {
-            System.out.println("存在错误，请检查。");
-            e.printStackTrace();
-        }
+        FastAutoGenerator.create(url, username, password)
+                .globalConfig(builder ->
+                        builder
+                                .disableOpenDir()
+                                .enableSwagger()
+                                .outputDir(JavaOutputDir))
+                .packageConfig(builder ->
+                        builder
+                                .parent(parent)
+                                .entity("model.entity")
+                                .pathInfo(Collections.singletonMap(OutputFile.xml, XMLOutputDir)))
+                .strategyConfig(builder ->
+                        builder
+                                .notLikeTable(new LikeTable("relation"))
+                                .controllerBuilder()
+                                .enableRestStyle()
+                                .entityBuilder()
+                                .enableLombok()
+                                .enableRemoveIsPrefix()
+                                .mapperBuilder()
+                                .mapperAnnotation(Mapper.class)
+                                .serviceBuilder()
+                                .convertServiceFileName((entityName -> entityName + ConstVal.SERVICE)))
+                .templateEngine(new FreemarkerTemplateEngine())
+                .templateConfig(builder ->
+                        builder
+                                .controller("/template/controller.java")
+                                .entity("/template/entity.java")
+                                .mapper("/template/mapper.java")
+                                .xml("/template/mapper.xml")
+                                .service("/template/service.java")
+                                .serviceImpl("/template/serviceImpl.java"))
+                .execute();
     }
 }
