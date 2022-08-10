3bd70257f27a6b3ea41f1f61cab5df0b3b624e7b
codewld <wang_longda@outlook.com>
2022-08-10 22:34:52 +0800

    feat[Web]: 集成tailwind css

--- I-Admin-Web/src/App.vue
+++ I-Admin-Web/src/App.vue
@@ -1,4 +1,4 @@
 <template>
-  <p>Hello World</p>
+  <p class="text-2xl text-teal-700">Hello World</p>
   <el-button type="primary">Primary</el-button>
 </template>
--- I-Admin-Web/src/main.ts
+++ I-Admin-Web/src/main.ts
@@ -4,4 +4,7 @@ import App from './App.vue'
 // 基础样式
 import '@/assets/css/base.css'
 
+// tailwind css
+import '@/assets/css/tailwind.css'
+
 createApp(App).mount('#app')
