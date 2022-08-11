630e0186451c2dafe96107b23d9987056424fb28
codewld <wang_longda@outlook.com>
2022-08-11 12:11:26 +0800

    feat[Server]: 增加通用的增删改查接口

--- I-Admin-Server/I-Admin-API/src/main/java/pers/codewld/iadmin/api/controller/AmAdminController.java
+++ I-Admin-Server/I-Admin-API/src/main/java/pers/codewld/iadmin/api/controller/AmAdminController.java
@@ -2,12 +2,14 @@ package pers.codewld.iadmin.api.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pers.codewld.iadmin.api.model.entity.AmAdmin;
+import pers.codewld.iadmin.crud.controller.BaseController;
 
 /**
  * 权限-后台用户 接口
  */
 @RestController
 @RequestMapping("/amAdmin")
-public class AmAdminController {
+public class AmAdminController extends BaseController<AmAdmin> {
 
 }
---
+++ I-Admin-Server/I-Admin-CRUD/src/main/java/pers/codewld/iadmin/crud/controller/BaseController.java
@@ -0,0 +1,51 @@
+package pers.codewld.iadmin.crud.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+/**
+ * 基本增删改查接口
+ */
+public class BaseController<T> {
+
+    @Resource
+    protected IService<T> baseService;
+
+    @ApiOperation("添加")
+    @PostMapping("")
+    public boolean add(T t) {
+        return baseService.save(t);
+    }
+
+    @ApiOperation("删除")
+    @DeleteMapping("/{id}")
+    public boolean del(@PathVariable String id) {
+        return baseService.removeById(id);
+    }
+
+    @ApiOperation("修改")
+    @PutMapping("")
+    public boolean update(T t) {
+        return baseService.updateById(t);
+    }
+
+    @ApiOperation("查询")
+    @GetMapping("/{id}")
+    public T get(@PathVariable String id) {
+        return baseService.getById(id);
+    }
+
+    @ApiOperation("分页查询")
+    @GetMapping("/page")
+    public Page<T> page(
+            @ApiParam("当前页数") Integer pageNum,
+            @ApiParam("每页条数") Integer pageSize) {
+        return baseService.page(new Page<>(pageNum, pageSize));
+    }
+
+}
