084238174c756c8ff1c8da9499116bf1eab54a87
codewld <wang_longda@outlook.com>
2022-08-13 20:58:50 +0800

    feat[Web]: 完善首页

--- I-Admin-Web/src/views/home/index.vue
+++ I-Admin-Web/src/views/home/index.vue
@@ -1,12 +1,27 @@
 <!--首页-->
 <script setup lang="ts">
+import { ref } from 'vue'
 import { useAccountStore } from '@/store'
+
+// -- Hello World相关 --
+let msg = ref('Hello World!')
+
 
 // -- 账户相关 --
 const accountStore = useAccountStore()
 </script>
 
 <template>
-  首页
-  <p>欢迎回来，{{accountStore.username}}</p>
+  <div class="h-full flex flex-col">
+    <div>
+      <p class="text-xl">
+        欢迎回来， {{accountStore.username}} ！
+      </p>
+    </div>
+    <div class="flex-1 flex justify-center items-center">
+      <p class="text-9xl font-bold tracking-wider italic select-none hover:text-blue-400">
+        {{ msg }}
+      </p>
+    </div>
+  </div>
 </template>
