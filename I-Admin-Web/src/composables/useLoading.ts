9a544610471bcfb67bd7933bd1d93492f499f406
codewld <wang_longda@outlook.com>
2022-08-22 18:35:42 +0800

    feat[Web]: 新增useLoading()

---
+++ I-Admin-Web/src/composables/useLoading.ts
@@ -0,0 +1,32 @@
+import { ElLoading } from 'element-plus'
+import 'element-plus/es/components/loading/style/css'
+
+/**
+ * 加载动画
+ */
+export default function useLoading() {
+
+  /** 加载动画实例 */
+  let loadingInstance: ReturnType<typeof ElLoading.service>
+
+  /**
+   * 开始加载动画
+   * @param config 配置
+   */
+  const startLoading = (config: Parameters<typeof ElLoading.service>[0]) => {
+    loadingInstance = ElLoading.service(config)
+  }
+
+  /**
+   * 停止加载动画
+   */
+  const endLoading = () => {
+    loadingInstance.close()
+  }
+
+  return {
+    startLoading,
+    endLoading
+  }
+
+}
