ad810078680024c5be53361c60dd994d462f8b5e
codewld <wang_longda@outlook.com>
2022-08-22 17:06:22 +0800

    feat[Server]: 配置跨域

--- I-Admin-Server/I-Admin-Common/src/main/java/pers/codewld/iadmin/common/config/WebConfiguration.java
+++ I-Admin-Server/I-Admin-Common/src/main/java/pers/codewld/iadmin/common/config/WebConfiguration.java
@@ -3,6 +3,7 @@ package pers.codewld.iadmin.common.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -17,6 +18,20 @@ public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         configurer.defaultContentType(MediaType.APPLICATION_JSON);
+    }
+
+    /**
+     * 跨域
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowCredentials(true)
+                .allowedOriginPatterns("http://127.0.0.1:[*]", "http://localhost:[*]")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders("*");
     }
 
 }
