c3f166a90d195845bc42e21197553a7c38f78735
codewld <wang_longda@outlook.com>
2022-08-30 02:14:28 +0800

    feat[Web]: 增加登录功能

---
+++ I-Admin-Web/src/api/account/index.ts
@@ -0,0 +1,11 @@
+import request from '@/api/index'
+import { Ref } from 'vue'
+
+const PREFIX = '/account'
+
+/**
+ * 登录
+ */
+export function rLogin(data: Ref<Account.loginParam>): Promise<string> {
+  return request(PREFIX + '/login', 'post', data)
+}
