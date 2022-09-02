72ae86b7dba7759b2f49a38d234e0aa9e2f4a8ba
codewld <wang_longda@outlook.com>
2022-09-02 22:32:16 +0800

    feat[Web]: 增加对jsx的支持

--- I-Admin-Web/vite.config.ts
+++ I-Admin-Web/vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,6 +17,9 @@ export default defineConfig({
       dirs: ['element-plus'],
       resolvers: [ElementPlusResolver()],
     }),
+    vueJsx({
+      // options are passed on to @vue/babel-plugin-jsx
+    })
   ],
   resolve: {
     alias: {
