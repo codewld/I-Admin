78ce3c384d3978b2cd9311a13e0713984bfd1203
codewld <wang_longda@outlook.com>
2022-09-03 22:01:13 +0800

    feat[Web]: 增加ICard卡片组件

---
+++ I-Admin-Web/src/components/iCard/i-card.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+// -- props --
+const props = defineProps({
+  /**
+   * 标题
+   */
+  title: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <el-card shadow="never">
+    <template #header>
+      <div class="h-10 flex justify-between items-center">
+        <!--标题区-->
+        <p class="text-base">
+          {{ props.title }}
+        </p>
+        <!--按钮区-->
+        <el-button-group>
+          <slot name="button"/>
+        </el-button-group>
+      </div>
+    </template>
+    <!--内容区-->
+    <slot/>
+  </el-card>
+</template>
