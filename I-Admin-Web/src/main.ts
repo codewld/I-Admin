d45182edfd3e98d538645f1bab91ef188159fd88
codewld <wang_longda@outlook.com>
2022-09-18 23:45:41 +0800

    feat[Web]: 支持指令方式的ElLoading

--- I-Admin-Web/src/main.ts
+++ I-Admin-Web/src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
 import store from '@/store'
+import { vLoading } from 'element-plus'
+import 'element-plus/theme-chalk/el-loading.css'
 
 
 // 基础样式
@@ -13,4 +15,5 @@ import '@/assets/css/tailwind.css'
 createApp(App)
   .use(router)
   .use(store)
+  .directive('loading', vLoading)
   .mount('#app')
