8075415ab01c1e56999839d6307889f6249fe5e7
codewld <wang_longda@outlook.com>
2022-09-21 23:16:19 +0800

    feat[Web]: 增加IStatus状态组件

---
+++ I-Admin-Web/src/components/iStatus/i-status.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { Check, Close } from '@element-plus/icons-vue'
+
+
+const props = defineProps({
+  /**
+   * 状态
+   */
+  status: {
+    type: Boolean,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="flex items-center justify-center">
+    <el-icon :size="20" :color="status ? 'green' : 'red'">
+      <Check v-if="status"/>
+      <Close v-else/>
+    </el-icon>
+  </div>
+</template>
---
+++ I-Admin-Web/src/components/iStatus/index.ts
@@ -0,0 +1,10 @@
+import IStatus from './i-status.vue'
+
+/**
+ * IStatus状态组件
+ * <p>特点:
+ * <p>1. 传入状态，显示成功/失败
+ * <p>Props:
+ * <p>1. status - 状态
+ */
+export default IStatus
