c33ff60c47304323c9ba900695812675534f493f
codewld <wang_longda@outlook.com>
2022-10-01 21:46:51 +0800

    refactor[Web]: 使用await优化代码; 调整代码

--- I-Admin-Web/src/components/iCrud/composables/useCrud.ts
+++ I-Admin-Web/src/components/iCrud/composables/useCrud.ts
@@ -24,30 +24,18 @@ export default function useCrud<T>(
   doLoad: () => void,
   beforeDoActionCallback?: crud.beforeDoActionCallback<T>) {
 
-  /**
-   * dialog是否可见
-   */
-  const dialogVisible = ref(false)
-
+  // -- 表单相关 --
   /**
    * 表单Ref
    */
   const formRef = ref()
 
-  /**
-   * 当前行主键值
-   */
-  const iCurrentRowKey = ref()
 
+  // -- 状态相关 --
   /**
-   * 当前行
+   * dialog是否可见
    */
-  const iCurrentRow = ref()
-
-  /**
-   * 表单数据
-   */
-  const formData = ref()
+  const dialogVisible = ref(false)
 
   /**
    * 当前正在进行的操作
@@ -70,45 +58,69 @@ export default function useCrud<T>(
     return actionMap[iAction.value]
   })
 
+
+  // -- 数据相关 --
+  /**
+   * 当前行主键值
+   */
+  const iCurrentRowKey = ref()
+
+  /**
+   * 当前行
+   */
+  const iCurrentRow = ref()
+
+  /**
+   * 表单数据
+   */
+  const formData = ref()
+
+
+  // -- 预操作相关 --
   /**
    * 是否正在查询完整行数据
    */
   const isGettingCurrentRow = ref(false)
 
   /**
+   * 查询完整行数据
+   * @throws 请求异常
+   */
+  const getCurrentRow = async () => {
+    try {
+      isGettingCurrentRow.value = true
+      iCurrentRow.value = await rGet(iCurrentRowKey.value)
+    } catch (e) {
+      ElMessage.warning(<string>e)
+      throw e
+    } finally {
+      isGettingCurrentRow.value = false
+    }
+  }
+
+  /**
    * 操作预处理
    * <p>1. 记录正在进行的操作
    * <p>2. 记录当前行主键值
-   * <p>3. 如果操作是修改或查看，根据当前行主键值查询完整行数据
    * @param action 操作
    * @param currentRowKey 当前行主键值
+   * @throws 有正在进行的操作
    */
   const beforeAction = async (action: crud.action, currentRowKey?: string) => {
-    // 记录正在进行的操作
     // 如果已经有正在进行的操作，则拒绝新操作
     if (iAction.value !== undefined) {
-      return false
+      throw '有正在进行的操作'
     }
+
+    // 记录正在进行的操作
     iAction.value = action
 
     // 记录当前行主键值
     iCurrentRowKey.value = currentRowKey
-
-    // 如果操作是修改或查看，根据当前行主键值查询完整行数据
-    if (['update', 'see'].includes(action)) {
-      try {
-        isGettingCurrentRow.value = true
-        iCurrentRow.value = await rGet(iCurrentRowKey.value)
-      } catch (err) {
-        console.log(err)
-      } finally {
-        isGettingCurrentRow.value = false
-      }
-    }
-
-    return true
   }
 
+
+  // -- 后操作相关 --
   /**
    * 关闭Dialog
    */
@@ -116,6 +128,23 @@ export default function useCrud<T>(
     dialogVisible.value = false
   }
 
+  /**
+   * 重置状态
+   * <p>应该在关闭dialog后调用此方法，避免界面异常
+   */
+  const resetAction = () => {
+    // 重置正在进行的操作
+    iAction.value = undefined
+
+    // 重置当前行主键值
+    iCurrentRowKey.value = undefined
+
+    // 重置当前行
+    iCurrentRow.value = undefined
+  }
+
+
+  // -- 回调相关 --
   /**
    * 操作执行前的处理
    * @param action 操作
@@ -133,35 +162,24 @@ export default function useCrud<T>(
     return true
   }
 
-  /**
-   * 重置状态
-   * <p>如果同时有dialog开启，应该先关闭dialog，当dialog关闭结束后再调用此方法，以避免因数据改变导致的关闭时界面异常
-   */
-  const resetAction = () => {
-    // 重置正在进行的操作
-    iAction.value = undefined
 
-    // 重置当前行主键值
-    iCurrentRowKey.value = undefined
-
-    // 重置当前行
-    iCurrentRow.value = undefined
-  }
-
+  // -- 增删改查相关 --
   /**
    * 准备添加
    */
   const handleAdd = async () => {
-    if (!await beforeAction('add')) {
-      return
-    }
+    try {
+      await beforeAction('add')
 
-    formData.value = {}
-    // 设置默认值
-    fieldList.forEach(item => {
-      formData.value[item.code] = item.formConf?.addDefault
-    })
-    dialogVisible.value = true
+      formData.value = {}
+      fieldList.forEach(item => {
+        formData.value[item.code] = item.formConf?.addDefault
+      })
+
+      dialogVisible.value = true
+    } catch (e) {
+      console.log(e)
+    }
   }
 
   /**
@@ -193,53 +211,56 @@ export default function useCrud<T>(
    * 准备删除
    */
   const handleDel = async (currentRowKey: string) => {
-    if (!await beforeAction('del', currentRowKey)) {
-      return
-    }
+    try {
+      await beforeAction('del', currentRowKey)
 
-    ElMessageBox.confirm(
-      '是否要进行删除?',
-      '删除确认',
-      {
-        confirmButtonText: '删除',
-        confirmButtonClass: 'el-button--danger',
-        cancelButtonText: '取消',
-        type: 'error',
-        draggable: true
-      }
-    )
-      .then(async () => {
-        if (!await beforeDoAction('del')) {
-          return
+      await ElMessageBox.confirm(
+        '是否要进行删除?',
+        '删除确认',
+        {
+          confirmButtonText: '删除',
+          confirmButtonClass: 'el-button--danger',
+          cancelButtonText: '取消',
+          type: 'error',
+          draggable: true
         }
+      )
 
-        rDel(iCurrentRowKey.value)
-          .then(() => {
-            ElMessage.success('操作成功')
-          })
-          .catch(err => {
-            ElMessage.warning(err)
-          })
-          .finally(() => {
-            doLoad()
-            resetAction()
-          })
-      })
-      .catch(() => {
-        resetAction()
-      })
+      if (!await beforeDoAction('del')) {
+        return
+      }
+
+      rDel(iCurrentRowKey.value)
+        .then(() => {
+          ElMessage.success('操作成功')
+        })
+        .catch(err => {
+          ElMessage.warning(err)
+        })
+        .finally(() => {
+          doLoad()
+          resetAction()
+        })
+    } catch (e) {
+      console.log(e)
+      resetAction()
+    }
   }
 
   /**
    * 准备修改
    */
   const handleUpdate = async (currentRowKey: string) => {
-    if (!await beforeAction('update', currentRowKey)) {
-      return
-    }
+    try {
+      await beforeAction('update', currentRowKey)
 
-    formData.value = { ...iCurrentRow.value }
-    dialogVisible.value = true
+      await getCurrentRow()
+      formData.value = { ...iCurrentRow.value }
+
+      dialogVisible.value = true
+    } catch (e) {
+      console.log(e)
+    }
   }
 
   /**
@@ -271,12 +292,16 @@ export default function useCrud<T>(
    * 准备查看
    */
   const handleSee = async (currentRowKey: string) => {
-    if (!await beforeAction('see', currentRowKey)) {
-      return
-    }
+    try {
+      await beforeAction('see', currentRowKey)
 
-    formData.value = { ...iCurrentRow.value }
-    dialogVisible.value = true
+      await getCurrentRow()
+      formData.value = { ...iCurrentRow.value }
+
+      dialogVisible.value = true
+    } catch (e) {
+      console.log(e)
+    }
   }
 
   return {
