f218f599a6e78fa31d5ace7f418a8b7e3bb93ac2
codewld <wang_longda@outlook.com>
2023-03-31 19:41:51 +0800

    refactor[Server]: "权限-后台用户"的密码改为后端加密

--- I-Admin-Server/I-Admin-API/src/main/java/pers/codewld/iadmin/api/controller/AmAdminController.java
+++ I-Admin-Server/I-Admin-API/src/main/java/pers/codewld/iadmin/api/controller/AmAdminController.java
@@ -1,9 +1,12 @@
 package pers.codewld.iadmin.api.controller;
 
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pers.codewld.iadmin.api.model.entity.AmAdmin;
 import pers.codewld.iadmin.common.controller.BaseController;
+import pers.codewld.iadmin.security.util.MD5Utils;
 
 /**
  * 权限-后台用户 接口
@@ -12,4 +15,17 @@ import pers.codewld.iadmin.common.controller.BaseController;
 @RequestMapping("/am-admin")
 public class AmAdminController extends BaseController<AmAdmin> {
 
+    @Override
+    public boolean add(@RequestBody @Validated AmAdmin amAdmin)  {
+        amAdmin.setPassword(MD5Utils.encode(amAdmin.getPassword()));
+        return super.add(amAdmin);
+    }
+
+    @Override
+    public boolean update(@RequestBody @Validated AmAdmin amAdmin)  {
+        if (amAdmin.getPassword() != null) {
+            amAdmin.setPassword(MD5Utils.encode(amAdmin.getPassword()));
+        }
+        return super.update(amAdmin);
+    }
 }
