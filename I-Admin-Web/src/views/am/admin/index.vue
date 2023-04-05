74b05baab51c699438a4f600849e24e2672d266b
codewld <wang_longda@outlook.com>
2023-04-05 15:09:04 +0800

    feat[Web]: "权限-后台用户"增加改密功能

--- I-Admin-Web/src/views/am/admin/index.vue
+++ I-Admin-Web/src/views/am/admin/index.vue
@@ -2,8 +2,12 @@
 <script setup lang="ts">
 import ICrud from '@/components/iCrud/i-crud.vue'
 import IStatus from '@/components/iStatus'
-import md5 from 'md5'
+import { EditPen } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+import 'element-plus/es/components/message/style/css'
 
+
+const crudRef = ref()
 
 const requestConf: crud.requestByPath = {
   path: '/am-admin',
@@ -13,7 +17,7 @@ const requestConf: crud.requestByPath = {
 const fieldList: crud.field[] = [
   {code: 'id', name: 'id', searchConf: {display: false}, tableConf: {display: false}, formConf: {add: false, update: false, see: false}},
   {code: 'username', name: '用户名', formConf: {rules: ['required']}},
-  {code: 'password', name: '密码', searchConf: {display: false}, tableConf: {display: false}, formConf: {rules: ['required']}},
+  {code: 'password', name: '密码', searchConf: {display: false}, tableConf: {display: false}, formConf: {update: false, see: false}},
   {code: 'nickName', name: '昵称'},
   {code: 'email', name: '邮箱'},
   {code: 'status', name: '启用状态', searchConf: {operator: 'EQ'}, formConf: {rules: ['required'], addDefault: 10}},
@@ -27,31 +31,48 @@ const orders: crud.order[] = [
   {field: 'create_time', type: 'DESC'}
 ]
 
-const beforeDoActionCallback: crud.beforeDoActionCallback<common.KVObj<any>> = async (
-    action,
-    currentRowKey,
-    currentRow,
-    formData) => {
-  if (['add', 'update'].includes(action)) {
-    // 对密码做MD5处理
-    const password = formData?.['password']
-    if (password) {
-      return {
-        formData: {...formData, ...{password: md5(password)}}
-      }
-    }
-  }
+
+// -- 修改密码相关 --
+/**
+ * 是否修改密码
+ */
+const changePassword = ref(false)
+
+/**
+ * 准备修改密码
+ */
+const handleChangePassword = () => {
+  changePassword.value = true
+  crudRef.value.handleAction('update')
+}
+
+/**
+ * 恢复状态为不修改密码
+ */
+const resetChangePassword = () => {
+  changePassword.value = false
 }
 </script>
 
 <template>
-  <i-crud :request-conf="requestConf" :field-list="fieldList" :orders="orders" :before-do-action-callback="beforeDoActionCallback">
+  <i-crud ref="crudRef" :request-conf="requestConf" :field-list="fieldList" :orders="orders" @after-action="resetChangePassword">
     <!--自定义搜索-->
     <template #search-item-status="{ row, placeholder }">
       <el-select v-model="row.status" :placeholder="placeholder" clearable class="w-full">
         <el-option :value="10" label="启用"></el-option>
         <el-option :value="0" label="禁用"></el-option>
       </el-select>
+    </template>
+    <!--自定义表格按钮-->
+    <template #table-button-rear="{ currentRow, currentRowKey, isLoading, hasAction }">
+      <el-button
+          :disabled="!currentRow || isLoading || hasAction"
+          @click="handleChangePassword"
+          :loading="changePassword"
+          :icon="EditPen"
+          type="info">
+        改密
+      </el-button>
     </template>
     <!--自定义表格-->
     <template #table-column-status="{ row }">
@@ -63,6 +84,13 @@ const beforeDoActionCallback: crud.beforeDoActionCallback<common.KVObj<any>> = a
         <el-option :value="10" label="启用"></el-option>
         <el-option :value="0" label="禁用"></el-option>
       </el-select>
+    </template>
+
+    <!--自定义修改密码表单-->
+    <template v-if="changePassword" #form="{ formData }">
+      <el-form-item prop="password" label="密码" :rules="{required: true, message: '请输入密码', trigger: 'blur'}">
+        <el-input v-model.trim="formData.password" placeholder="请输入密码"/>
+      </el-form-item>
     </template>
   </i-crud>
 </template>
