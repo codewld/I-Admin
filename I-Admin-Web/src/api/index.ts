141e62076c2fe46ecb28da0470f1404fb775b915
codewld <wang_longda@outlook.com>
2023-04-10 19:16:38 +0800

    feat[Web]: 避免发送重复请求

--- I-Admin-Web/src/api/index.ts
+++ I-Admin-Web/src/api/index.ts
@@ -49,6 +49,12 @@ instance.interceptors.response.use(
 )
 
 /**
+ * 请求Map
+ * <p>将正在进行中的请求保存，用于避免重复请求
+ */
+const requestMap: Map<string, Promise<unknown>> = new Map()
+
+/**
  * 发送网络请求
  * @param path 请求路径
  * @param method HTTP方法
@@ -56,9 +62,10 @@ instance.interceptors.response.use(
  * @param data 请求体参数
  * @param isLoading 是否展示加载动画
  * @param loadingText 加载动画的提示文字
+ * @param forceRequest 强制请求  [为true时，即使有正在进行的相同请求，也会始终发起请求；默认为false]
  * @return Promise 响应数据
  */
-function request<P, T, R>(path: string, method: Method = 'get', params?: P, data?: T, isLoading: boolean = false, loadingText: string = '加载中'): Promise<R> {
+function request<P, T, R>(path: string, method: Method = 'get', params?: P, data?: T, isLoading: boolean = false, loadingText: string = '加载中', forceRequest: boolean = false): Promise<R> {
 
   const requestConfig: AxiosRequestConfig = {
     url: path,
@@ -68,7 +75,13 @@ function request<P, T, R>(path: string, method: Method = 'get', params?: P, data
   requestConfig.params = unref(params)
   requestConfig.data = unref(data)
 
-  return new Promise((resolve, reject) => {
+  const requestConfigStr = JSON.stringify(requestConfig)
+
+  if (!forceRequest && requestMap.has(requestConfigStr)) {
+    return <Promise<R>>requestMap.get(requestConfigStr)
+  }
+
+  const request: Promise<R> = new Promise((resolve, reject) => {
 
     const { startLoading, endLoading } = useLoading()
     const loadingConfig = {
@@ -85,8 +98,13 @@ function request<P, T, R>(path: string, method: Method = 'get', params?: P, data
       })
       .finally(() => {
         isLoading && endLoading()
+        requestMap.delete(requestConfigStr)
       })
   })
+
+  requestMap.set(requestConfigStr, request)
+
+  return request
 }
 
 export default request
